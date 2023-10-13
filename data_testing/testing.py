94b8509d436bd05a840eee816918b8aa3623169a
Matt Moore <Moore.MatthewPaul@gmail.com>
2023-10-13 00:35:12 -0400

    testing.py is working

--- data_testing/testing.py
+++ data_testing/testing.py
@@ -1,12 +1,38 @@
 import json
+import os
+import csv
+
+
+from pydfs_lineup_optimizer import get_optimizer, Site, Sport
+optimizer = get_optimizer(Site.DRAFTKINGS_CAPTAIN_MODE, Sport.FOOTBALL)
+
+#If there is a fail here, watch out for empty strings in the csv, as they will cause the parser to search too far
+optimizer.load_players_from_csv("../SD_Proj.csv")
+
+N=50
+lineups = optimizer.optimize(n=N)
 
 
 
-with open('../CONTEST_DEFS.json') as f:
-    data = json.load(f)
+# Specify the file name for the CSV export
+csv_filename = 'lineups.csv'
 
-print(data['DRAFTKINGS']['NFL']['CLASSIC']['POSITION_MIN'])
-print(data['DRAFTKINGS']['NFL']['CLASSIC']['POSITION_MAX'])
+# Export lineups to a CSV file
+with open(csv_filename, 'w', newline='') as csvfile:
+    lineup_writer = csv.writer(csvfile)
+    
+    # Write the header row with column names
+    lineup_writer.writerow(['CPTN', 'FLEX', 'FLEX', 'FLEX', 'FLEX', 'FLEX'])
+
+    # Write each lineup to the CSV file
+    for lineup in lineups:
+        lineup_writer.writerow([player.full_name for player in lineup.players])
+
+print(f"Lineups exported to {csv_filename}")
+
+
+
+
 
 
 
---
+++ debugging.py
@@ -0,0 +1,21 @@
+#https://www.youtube.com/watch?v=KEdq7gC_RTA&ab_channel=BorisPaskhaver
+
+print("Welcome to my program")
+a_gbl = 10
+b_gbl = 20
+c_gbl = 30
+
+
+def do_fun_stuff():
+    a=20
+    print("Hello")
+    print("Goodbye")
+    a=25
+    c=40
+    d=50
+    return a
+
+final_value = do_fun_stuff()
+print(do_fun_stuff())
+
+
